027ee3fd5fa85f6d495de1294efdfb4de57d7fcf
jin_young <samus531531@gmail.com>
2026-06-30 12:18:53 +0900

    edit_post, edit_post_process 파일 여러개 업로드 고침. 아맞다 guestbookgit add .

--- 26_board/db.php
+++ 26_board/db.php
@@ -7,3 +7,18 @@ $connection = mysqli_connect("localhost", "root", "qwer1234", "board26");
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+// <?php
+
+// $host = "localhost";
+// $user = "board26_user";
+// $password = "c96gef8bdd42113";
+// $dbname = "board26";
+
+// // db 연결
+// $connection = mysqli_connect($host, $user, $password, $dbname);
+
+// // 연결 확인
+// if (!$connection) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
